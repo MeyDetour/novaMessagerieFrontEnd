1fbd5d474b51ce10f1544ee7b47610b4eefd8c9e
mey <meydetour@gmail.com>
2023-11-16 16:14:36 +0100

    sign up sign in fil

--- main.js
+++ main.js
@@ -11,26 +11,149 @@ addStyleRule('.centered', `display :flex ; justify-content : center ; align-item
 
 
 //=======================================================================
-let nom = document.querySelector('#username')
-let mdp = document.querySelector('#password')
-let error = document.querySelector('.error')
+let token = null
+let error = ""
+let error2 = ""
+//checker les cookies du navigateur et le rempalcer
+//formulaire acceptation coockie
+let content = document.querySelector('.containerFond')
 const baseUrl = "https://b1messenger.imatrythis.tk/"
-const buttonLogin = document.getElementById('buttonLogin')
-let bouttonSend = document.querySelector('.bi-send')
+let nom = ""
+let mdp = ""
+nomsignup = ""
+mdpsignup = ""
+run()
 
-buttonLogin.addEventListener('click', () => {
+function run() {
+    console.log('run')
+    if (!token) {
+        renderForm()
+    } else {
+        getMessages().then(response => {
+            renderMessage(response)
+            scrollY()
+        })
+    }
 
-    let token = messengerLogin(nom.value,mdp.value)
-})
-bouttonSend.addEventListener('click', () => {
-    let inputPostMessage = document.querySelector('.MsgInput')
-    postMessage(inputPostMessage.value)
-})
+}
 
-async function messengerLogin(nom,mdp){
+function scrollY() {
+    const filD = document.querySelector('.messages')
+    filD.scrollTo(0, filD.scrollHeight);
+}
+
+function renderForm() {
+    let form = `
+    <div class="loginform">
+        <p class="heading">Login</p>
+        <div class="inputContainer">
+            <svg class="inputIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#2e2e2e"
+                 viewBox="0 0 16 16">
+                <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
+            </svg>
+            <input type="text" class="inputField" id="username" placeholder="Username">
+        </div>
+        <div class="inputContainer">
+            <svg class="inputIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#2e2e2e"
+                 viewBox="0 0 16 16">
+                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
+            </svg>
+            <input type="password" class="inputField" id="password" placeholder="Password">
+        </div>
+        <button id="buttonLogin">Submit</button>
+        <span class="error"></span>
+        <a class="loginpage" id="signup" href="#">Signup</a>
+        <a class="forgotLink" id="forgotmdp" href="#">Forgot your password?</a>
+    </div>
+    `
+    render(form)
+
+    error = document.querySelector('.error')
+    const buttonLogin = document.querySelector('#buttonLogin')
+    const signup = document.querySelector('#signup')
+    const forgotmdp = document.querySelector('.forgotmdp')
+
+    signup.addEventListener('click', () => {
+        renderSignup()
+    })
+
+    buttonLogin.addEventListener('click', () => {
+        nom = document.querySelector('#username')
+        mdp = document.querySelector('#password')
+        getToken(nom.value, mdp.value)
+
+    })
+
+}
+
+function renderSignup() {
+    let form = `
+ <div class="card">
+        <h4 class="title">Sign up!</h4>
+
+        <div class="field">
+            <svg class="input-icon" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+                <path d="M207.8 20.73c-93.45 18.32-168.7 93.66-187 187.1c-27.64 140.9 68.65 266.2 199.1 285.1c19.01 2.888 36.17-12.26 36.17-31.49l.0001-.6631c0-15.74-11.44-28.88-26.84-31.24c-84.35-12.98-149.2-86.13-149.2-174.2c0-102.9 88.61-185.5 193.4-175.4c91.54 8.869 158.6 91.25 158.6 183.2l0 16.16c0 22.09-17.94 40.05-40 40.05s-40.01-17.96-40.01-40.05v-120.1c0-8.847-7.161-16.02-16.01-16.02l-31.98 .0036c-7.299 0-13.2 4.992-15.12 11.68c-24.85-12.15-54.24-16.38-86.06-5.106c-38.75 13.73-68.12 48.91-73.72 89.64c-9.483 69.01 43.81 128 110.9 128c26.44 0 50.43-9.544 69.59-24.88c24 31.3 65.23 48.69 109.4 37.49C465.2 369.3 496 324.1 495.1 277.2V256.3C495.1 107.1 361.2-9.332 207.8 20.73zM239.1 304.3c-26.47 0-48-21.56-48-48.05s21.53-48.05 48-48.05s48 21.56 48 48.05S266.5 304.3 239.1 304.3z"></path>
+            </svg>
+            <input autocomplete="off" id="usernamesignup" placeholder="Username" class="input-field" name="logemail"
+                   type="email">
+        </div>
+        <div class="field">
+            <svg class="input-icon" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+                <path d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z"></path>
+            </svg>
+            <input autocomplete="off" id="mdpsignup" placeholder="Password" class="input-field" name="logpass"
+                   type="password">
+        </div>
+                  <span class="error2"></span>
+        <button class="btn submitsignup" type="submit">Sign up</button>
+
+      <a  id="loginfromsignup" href="#">Login</a>
+    </div>
+`
+    render(form)
+    error2 = document.querySelector('.error2')
+    let submitsignup = document.querySelector('.submitsignup')
+    let btnlogin = document.querySelector('#loginfromsignup')
+    nomsignup = document.querySelector('#usernamesignup')
+    mdpsignup = document.querySelector('#mdpsignup')
+
+    submitsignup.addEventListener('click',()=>{
+        register(nomsignup.value,mdpsignup.value)
+    })
+
+    btnlogin.addEventListener('click',()=>{
+        renderForm()
+    })
+
+
+}
+
+async function register(name, mdpasse) {
+fetch(`${baseUrl}register`,profilParametreFetch(name,mdpasse))
+    .then(response =>response.json())
+    .then(data =>{
+        console.log(name,mdpasse)
+        console.log(data)
+        if (data === "username already taken"){
+            error2.textContent = 'Pseudo déja pris ! Veuillez réessayer merci :)'
+            setTimeout(() => {
+                error2.textContent = ""
+            }, 2000)
+            nomsignup.value = ""
+            mdpsignup.value = ""
+        }
+        else{
+            getToken(name,mdpasse)
+
+        }
+    })
+}
+
+function profilParametreFetch(name, mdpasse) {
     const utilisateur = {
-        username: nom,
-        password: mdp,
+        username: name,
+        password: mdpasse,
     }
     const messengerLogin =
         {
@@ -38,26 +161,73 @@ async function messengerLogin(nom,mdp){
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify(utilisateur)
         }
+    return messengerLogin
+}
+
+async function getToken(name, mdpasse) {
 
     //login
-    fetch(`${baseUrl}login`, messengerLogin)
+    return await fetch(`${baseUrl}login`, profilParametreFetch(name, mdpasse))
         .then(response => response.json())
         .then(data => {
+            //data = token
             //mauvais pseudo&mdp
-            if (data['message'] === "Invalid credentials.") {
+
+            if (data.message === "Invalid credentials.") {
+
                 error.textContent = 'Erreur ! Veuillez réessayer merci :)'
                 setTimeout(() => {
                     error.textContent = ""
                 }, 2000)
+                nom.value = ""
+                mdp.value = ""
             } else {
-                return date.token
+                console.log(token)
+                token = data.token
+                run()
+                //stocker dans les cookies
             }
         })
 }
 
 
-async function recupererMessage() {
+function renderMessage(listeMessage) {
+    let fil = `
+      <div class="filDiscussion">
+        <div class="messages">
 
+        </div>
+        <div class="postMessage">
+            <input title="Write Message" tabindex="i" pattern="\\d+" placeholder="Message.." class="msgInput"
+                   type="text">
+            <i class="bi bi-send"></i>
+            <i class="bi bi-arrow-clockwise refreshBtn"></i>
+        </div>
+    </div>
+    `
+    render(fil)
+    let bouttonSend = document.querySelector('.bi-send')
+    let boutonRefresh = document.querySelector('.refreshBtn')
+    const messageAEnvoyer = document.querySelector('.msgInput')
+    bouttonSend.addEventListener('click', () => {
+        console.log(messageAEnvoyer.value)
+        postMessage(messageAEnvoyer.value)
+        messageAEnvoyer.value = ""
+    })
+
+    boutonRefresh.addEventListener('click', () => {
+        run()
+    })
+
+
+    listeMessage.forEach((message) => {
+        addMessage(message)
+    })
+
+
+}
+
+async function getMessages() {
     //get message
     const messengerMessage =
         {
@@ -67,27 +237,32 @@ async function recupererMessage() {
                 'Authorization': `Bearer ${token}`
             },
         }
-
-
-    await fetch(`${baseUrl}api/messages`, messengerMessage)
+    return await fetch(`${baseUrl}api/messages`, messengerMessage)
         .then(response => response.json())
         .then(data => {
+            if (data.message === "Invalid credentials.") {
+                renderForm()
+            } else {
+                return data
+            }
 
-            document.querySelector('.filDiscussion').classList.toggle('d-none')
-            document.querySelector('.loginform').classList.toggle('d-none')
-            afficheMessage(data)
+
         })
-
 }
 
+function identifier(usernom) {
+    if (nom.value === usernom) {
+        return "Vous"
+    }
+    return usernom
+}
 
-async function afficheMessage(listemessage) {
-    listemessage.forEach((message) => {
-        console.log(message)
-        document.querySelector('.messages').innerHTML += `                    
+function addMessage(message) {
+    const zoneMessage = document.querySelector('.messages')
+    zoneMessage.innerHTML += `                    
                               <div class="task" draggable="true">
                                 <div class="tags">
-                                  <span class="tag">${message['author']['username']}</span>
+                                  <span class="tag">${identifier(message['author']['username'])}</span>
                                   <button class="options">
                                     <svg xml:space="preserve" viewBox="0 0 41.915 41.916" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" id="Capa_1" version="1.1" fill="#000000"><g stroke-width="0" id="SVGRepo_bgCarrier"></g><g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M11.214,20.956c0,3.091-2.509,5.589-5.607,5.589C2.51,26.544,0,24.046,0,20.956c0-3.082,2.511-5.585,5.607-5.585 C8.705,15.371,11.214,17.874,11.214,20.956z"></path> <path d="M26.564,20.956c0,3.091-2.509,5.589-5.606,5.589c-3.097,0-5.607-2.498-5.607-5.589c0-3.082,2.511-5.585,5.607-5.585 C24.056,15.371,26.564,17.874,26.564,20.956z"></path> <path d="M41.915,20.956c0,3.091-2.509,5.589-5.607,5.589c-3.097,0-5.606-2.498-5.606-5.589c0-3.082,2.511-5.585,5.606-5.585 C39.406,15.371,41.915,17.874,41.915,20.956z"></path> </g> </g> </g></svg>
                                   </button>
@@ -108,29 +283,39 @@ async function afficheMessage(listemessage) {
                                   </div>
                     `
 
-    })
 }
-async function postMessage(message) {
+
+
+function postMessage(message) {
+    console.log(token, message)
     const messengerMessage =
         {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Content-type': 'application/json',
                 'Authorization': `Bearer ${token}`
             },
-            body: {
-                'content': `message.value`
-            }
+            body: JSON.stringify({
+                'content': `${message}`
+            })
         }
 
-    fetch(`${baseUrl}messages/new`, messengerMessage)
+    fetch('https://b1messenger.imatrythis.tk/api/messages/new', messengerMessage)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
-            recupererMessage(token)
+            //"ok"
+
+            run()
+
 
         })
 }
 
+
+function render(contenu) {
+    content.innerHTML = ""
+    content.innerHTML = contenu
+
+}
 
 
